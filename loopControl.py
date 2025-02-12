3510fa25c55253e8d891654ec1c2252a57a35a69
Ali Onder Ergun <43433494+onderergun@users.noreply.github.com>
2025-02-12 15:32:05 +0300

    Update loopControl.py

--- loopControl.py
+++ loopControl.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
-
 from jsonrpclib import Server
+import time
 
 def main():
     api = Server("unix:/var/run/command-api.sock")
-    logs = api.runCmds(1, ["show logging last 3 minutes"],"text" )
+    logs = api.runCmds(1, ["show logging last 1 minutes"],"text" )
     loglines = logs[0]["output"].split('\n')
-    loopedinterfaces= []
-    excludedinterfaces = ["Port-Channel1"]
+    excludedinterfaces = ["Cpu","Vxlan1","Port-Channel100"]
+    loopedinterfaces = []
     for line in loglines:
         if "ETH-4-HOST_FLAPPING:" in line:
             linesplit = line.split(" ")
             for word in linesplit:
                 if ("Ethernet" in word or "Port-Channel" in word) and (word not in excludedinterfaces):
                     loopedinterfaces.append(word)
-    commands = []
+
     for interface in loopedinterfaces:
-        commands.append("errdisable test interface " + interface)
-    if len(commands) > 0:
-        api.runCmds(1, commands)
+        intcountersfirst = api.runCmds(1, ["show interfaces counters"],"json" )[0]["interfaces"]
+        time.sleep(10)
+        intcounterssecond = api.runCmds(1, ["show interfaces counters"],"json" )[0]["interfaces"]
+        if intcounterssecond[interface]["inBroadcastPkts"] - intcountersfirst[interface]["inBroadcastPkts"] > 10000:
+            api.runCmds(1, ["errdisable test interface " + interface],"json" )
+            api.runCmds(1,["send log message "+ interface+ " ERRDISABLED BY SCRIPT BECAUSE OF LOOP"])
+
 if __name__ == "__main__":
     main()
 
 """
-event-handler loopcontrol
-   action bash python /mnt/flash/loopControl.py
-   delay 10
-   !
-   trigger on-logging
-      regex EVPN-3-BLACKLISTED_DUPLICATE_MAC
-
 management api http-commands
    protocol unix-socket
    no shutdown
+
+event-handler loopcontrolvlan
+   action bash python /mnt/flash/loopControl.py
+   repeat interval 60
+   threshold 60 count 5
+   !
+   trigger on-logging
+      regex ETH-4-HOST_(FLAPPING)
 """
